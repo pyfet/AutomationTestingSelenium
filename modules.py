dc6bf3a1844ef8768d2d5bb873c3abb64f9a5976
Yemdjih Kazer Nasser <xurror@localhost.localdomain>
2019-12-28 14:52:12 +0100

    add TODOs for modules

--- modules.py
+++ modules.py
@@ -6,9 +6,15 @@ class Module():
         self.driver = driver
         self.authenticate = Authenticate(self.driver)
 
+    # Interaction
+    def draggable(self):
+        #TODO: Implement tests for the draggable module
+        driver = self.driver        
+        authenticate(driver)
+        time.sleep(3)  
+
     def droppable(self):
         driver = self.driver        
-
         authenticate(driver)
         time.sleep(3)
 
@@ -26,4 +32,99 @@ class Module():
 
         # Leave the frame
         driver.switch_to_default_content()
-        
+
+    def resizable(self):
+        #TODO: Implement tests for the resizable module
+        driver = self.driver        
+        authenticate(driver)
+        time.sleep(3) 
+    
+    def selectable(self):
+        #TODO: Implement tests for the selectable module
+        driver = self.driver        
+        authenticate(driver)
+        time.sleep(3) 
+
+    def sortable(self):
+        #TODO: Implement tests for the sortable module
+        driver = self.driver        
+        authenticate(driver)
+        time.sleep(3) 
+
+    # Widget
+    def accordion(self):
+        #TODO: Implement tests for the accordion module
+        driver = self.driver        
+        authenticate(driver)
+        time.sleep(3) 
+
+    def autocomplete(self):
+        #TODO: Implement tests for the autocomplete module
+        driver = self.driver        
+        authenticate(driver)
+        time.sleep(3) 
+
+    def datepicker(self):
+        #TODO: Implement tests for the datepicker module
+        driver = self.driver        
+        authenticate(driver)
+        time.sleep(3) 
+
+    def menu(self):
+        #TODO: Implement tests for the menu module
+        driver = self.driver        
+        authenticate(driver)
+        time.sleep(3) 
+
+    def slider(self):
+        #TODO: Implement tests for the slider module
+        driver = self.driver        
+        authenticate(driver)
+        time.sleep(3) 
+
+    def tabs(self):
+        #TODO: Implement tests for the tabs module
+        driver = self.driver        
+        authenticate(driver)
+        time.sleep(3) 
+
+    def tooltip(self):
+        #TODO: Implement tests for the tooltip module
+        driver = self.driver        
+        authenticate(driver)
+        time.sleep(3)
+
+    # Frames and Windows
+    def frames_and_windows(self):
+        #TODO: Implement tests for the frames and windows module
+        driver = self.driver        
+        authenticate(driver)
+        time.sleep(3)
+
+    # Dynamic element
+    def submit_button_clicked(self):
+        #TODO: Implement tests for the submit button clicked module
+        driver = self.driver        
+        authenticate(driver)
+        time.sleep(3)
+
+    def dropdown(self):
+        #TODO: Implement tests for the dropdown module
+        driver = self.driver        
+        authenticate(driver)
+        time.sleep(3)
+
+    # Registration
+    def registration(self):
+        #TODO: Implement tests for the registration module
+        driver = self.driver        
+        authenticate(driver)
+        time.sleep(3)
+
+    # Alert
+    def alert(self):
+        #TODO: Implement tests for the alert module
+        driver = self.driver        
+        authenticate(driver)
+        time.sleep(3)
+ 
